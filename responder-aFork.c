1301af2a59e3e3e44b183c5a93998e33fcc68c8e
Ian Mitchell <177580977+IanMitchellOR-UAM@users.noreply.github.com>
2024-09-18 10:33:03 -0600

    Update responder-aFork.c

--- responder-aFork.c
+++ responder-aFork.c
@@ -1,112 +1,128 @@
-#include <fcntl.h>
-#include <signal.h>
+/***
+sudo addusur nombre_usuario
+El programa exige que la variable de entorno LOGNAME estÃ© correctamente
+inicializada con el nombre del usuario. Esto se puede conseguir incluyendo las dos
+lÃ­neas siguientes en el fichero .profile de configuraciÃ³n de nuestro intÃ©rprete de
+Ã³rdenes:
+LOGNAME='logname'
+export LOGNAME
+***/
+ 
 #include <stdio.h>
-#include <stdlib.h>      // Para exit() y getenv()
-#include <string.h>      // Para strcmp()
-#include <sys/select.h>  // Para select()
-#include <sys/stat.h>
+#include <stdlib.h>   // Se incluye para la función exit() y getenv()
+#include <string.h>   // Se incluye para la función strcmp()
+#include <signal.h>
+#include <fcntl.h>
 #include <sys/types.h>
-#include <unistd.h>  // Para read(), write(), close()
-
+#include <sys/stat.h>
+#include <unistd.h>   // Se incluye para las funciones read(), write() y close()
+ 
 #define MAX 256
-#define EQ(str1, str2) \
-    (strcmp((str1), (str2)) == 0)  // Macro para comparar dos cadenas
-
-int fifo_12[MAX], fifo_21[MAX];
-char nombre_fifo_12[MAX][MAX], nombre_fifo_21[MAX][MAX];
+ 
+/* Macro para comparar dos cadenas de caracteres */
+#define EQ(str1, str2) (strcmp((str1), (str2)) == 0)
+ 
+/* Descriptores de fichero de las tuberías mediante las que nos vamos a comunicar */
+int fifo_12, fifo_21;
+char nombre_fifo_12[MAX], nombre_fifo_21[MAX];
+ 
+/* Array para leer los mensajes */
 char mensaje[MAX];
-int num_usuarios;
-
+ 
+/* Declaración de funciones */
 void fin_de_transmision(int sig);
-
+void ter_proc();
+ 
 int main(int argc, char *argv[]) {
     char *logname;
-
-    // Análisis de los argumentos de la línea de órdenes
-    if (argc < 2) {
-        fprintf(stderr, "Forma de uso: %s usuario1 usuario2 ... usuarioN\n",
-                argv[0]);
+ 
+    /* Análisis de los argumentos de la línea de órdenes */
+    if (argc != 2) {
+        fprintf(stderr, "Forma de uso: %s usuario\n", argv[0]);
         exit(-1);
     }
-
-    num_usuarios = argc - 1;
-
-    // Lectura del nombre del usuario
+ 
+    /* Lectura del nombre del usuario */
     logname = getenv("LOGNAME");
-
-    // Comprobación para que un usuario no se responda a sí mismo
-    for (int i = 1; i < argc; i++) {
-        if (EQ(logname, argv[i])) {
-            fprintf(stderr, "Comunicación con uno mismo no permitida\n");
-            exit(0);
-        }
+ 
+    /* Comprobación para que un usuario no se responda a sí mismo */
+    if (EQ(logname, argv[1])) {
+        fprintf(stderr, "Comunicación con uno mismo no permitida\n");
+        exit(0);
     }
-
-    // Apertura de las tuberías
-    for (int i = 1; i < argc; i++) {
-        sprintf(nombre_fifo_12[i - 1], "/tmp/%s_%s", argv[i], logname);
-        sprintf(nombre_fifo_21[i - 1], "/tmp/%s_%s", logname, argv[i]);
-
-        if ((fifo_12[i - 1] = open(nombre_fifo_12[i - 1], O_RDONLY)) == -1 ||
-            (fifo_21[i - 1] = open(nombre_fifo_21[i - 1], O_WRONLY)) == -1) {
-            perror("Error al abrir tuberías");
-            exit(-1);
-        }
+ 
+    /* Formación del nombre de las tuberías de comunicación */
+    sprintf(nombre_fifo_12, "/tmp/%s_%s", argv[1], logname);
+    sprintf(nombre_fifo_21, "/tmp/%s_%s", logname, argv[1]);
+ 
+    /* Apertura de las tuberías */
+    if ((fifo_12 = open(nombre_fifo_12, O_RDONLY)) == -1 ||
+        (fifo_21 = open(nombre_fifo_21, O_WRONLY)) == -1) {
+        perror("Error al abrir las tuberías");
+        exit(-1);
     }
-
-    // Armamos el manejador de la señal SIGINT
+ 
+    /* Armamos el manejador de la señal SIGINT */
     signal(SIGINT, fin_de_transmision);
-
+ 
     pid_t pid;
-    if ((pid = fork()) == 0) {  // Proceso hijo: Recepción de mensajes
-        fd_set readfds;
-        int max_fd = 0;
-
-        // Configurar el conjunto de descriptores
-        for (int i = 0; i < num_usuarios; i++) {
-            if (fifo_12[i] > max_fd) {
-                max_fd = fifo_12[i];
+    pid_t padre = getpid();
+    /* Proceso hijo: recepción de mensajes */
+    if ((pid = fork()) == 0) {
+        signal(SIGTERM, ter_proc);
+	do {
+            printf("==> ");
+            fflush(stdout);
+            if (read(fifo_12, mensaje, MAX) <= 0) {
+                perror("Error al leer el mensaje");
+                exit(-1);
             }
-        }
-
-        do {
-            FD_ZERO(&readfds);
-            for (int i = 0; i < num_usuarios; i++) {
-                FD_SET(fifo_12[i], &readfds);
-            }
-
-            // Esperar mensajes
-            select(max_fd + 1, &readfds, NULL, NULL, NULL);
-
-            // Leer mensajes de los usuarios disponibles
-            for (int i = 0; i < num_usuarios; i++) {
-                if (FD_ISSET(fifo_12[i], &readfds)) {
-                    read(fifo_12[i], mensaje, MAX);
-                    printf("Mensaje de %s: %s", argv[i + 1], mensaje);
-                }
-            }
-        } while (!EQ(mensaje, "corto\n") && !EQ(mensaje, "cambio\n"));
-    } else {  // Proceso padre: Envío de mensajes
+	    if(EQ(mensaje, "corto\n")){
+	    	kill(padre, SIGTERM);
+		break;
+	    }
+            printf("%s", mensaje);
+        } while (!EQ(mensaje, "corto\n"));
+    } 
+    /* Proceso padre: envío de mensajes */
+    else {
+	signal(SIGTERM, ter_proc);
         do {
             printf("<== ");
+	    fflush(stdin);
             fgets(mensaje, sizeof(mensaje), stdin);
-            for (int i = 0; i < num_usuarios; i++) {
-                write(fifo_21[i], mensaje, strlen(mensaje) + 1);
-            }
+            write(fifo_21, mensaje, strlen(mensaje) + 1);
+	    if(EQ(mensaje, "corto\n")) kill(pid, SIGTERM);
         } while (!EQ(mensaje, "corto\n"));
     }
-
-    fin_de_transmision(0);
+ 
+    printf("FIN DE TRANSMISIÓN.\n");
+    close(fifo_12);
+    close(fifo_21);
+    unlink(nombre_fifo_12);
+    unlink(nombre_fifo_21);
+ 
     return 0;
 }
-
+ 
+/***
+fin_de_transmision: rutina de tratamiento de la señal SIGINT. Al pulsar Ctrl+C
+entramos en esta rutina, que se encarga de enviar el mensaje "corto\n" al usuario
+con el que estamos hablando.
+***/
 void fin_de_transmision(int sig) {
     sprintf(mensaje, "corto\n");
-    for (int i = 0; i < num_usuarios; i++) {
-        write(fifo_21[i], mensaje, strlen(mensaje) + 1);
-        close(fifo_12[i]);
-        close(fifo_21[i]);
-    }
+    write(fifo_21, mensaje, strlen(mensaje) + 1);
     printf("FIN DE TRANSMISIÓN.\n");
+    close(fifo_12);
+    close(fifo_21);
+    exit(0);
+}
+ 
+void ter_proc(){
+    sprintf(mensaje, "corto\n");
+    write(fifo_21, mensaje, strlen(mensaje) + 1);
+    close(fifo_12);
+    close(fifo_21);
     exit(0);
 }
